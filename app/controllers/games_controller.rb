d6fe967eb28f3c43d46943829b4945c6577e9e58
Pedro Cobos <pedro.cobos@live.com>
2020-11-08 13:01:19 -0600

    added pry-byebug to gemfile, updated games controller score action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,22 @@
 class GamesController < ApplicationController
+  attr_reader :letters
+
   def new
     @letters = (("A".."Z").to_a * 2).sample(9)
   end
 
   def score
+    # Grab the word sent by the user
+    @word = params[:word]
+    # Check if the word was made using the letters contained in the grid
+    # Check if the word is english
+    # If so, we say congrats
+    # Else we 
+  end
+
+  private
+
+  def english_word?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
   end
 end
